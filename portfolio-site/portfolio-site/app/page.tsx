888d2dc86661d55dfba2b4a8c22dfad2902b805b
mahir213 <hajdarevicm10@gmail.com>
2025-02-12 23:28:15 +0100

    + UI components

--- portfolio-site/portfolio-site/app/page.tsx
+++ portfolio-site/portfolio-site/app/page.tsx
@@ -1,11 +1,38 @@
-import Image from "next/image";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 import Hero from "../components/Hero";
+import { FaHome, FaUser, FaProjectDiagram, FaEnvelope } from "react-icons/fa";
+import Grid from "@/components/Grid";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div>
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={
+          [
+            {
+              name: "Home",
+              link: "#",
+              icon: <FaHome />,
+            },
+            {
+              name: "About",
+              link: "#about",
+              icon: <FaUser />,
+            },
+            {
+              name: "Projects",
+              link: "#projects",
+              icon: <FaProjectDiagram />,
+            },
+            {
+              name: "Contact",
+              link: "#contact",
+              icon: <FaEnvelope />,
+            },
+          ]
+        }/>
        <Hero/>
+       <Grid/>
        </div>
     </main>
   );
---
+++ portfolio-site/portfolio-site/components/Grid.tsx
@@ -0,0 +1,24 @@
+
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+
+const Grid = () => {
+  return (
+    <section id="about">
+      <BentoGrid className="w-full py-20">
+        
+        {[
+          { title: 'Title', description: 'Desc1', id:1 }
+        ].map((item) => (
+            <BentoGridItem
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+            />
+        ))}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
