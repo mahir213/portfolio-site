fb1d1d7f3f8feb2d36bc9480bd3e134280310a6f
mahir213 <hajdarevicm10@gmail.com>
2025-02-14 11:59:49 +0100

    data + ui upgrade

--- portfolio-site/portfolio-site/components/Grid.tsx
+++ portfolio-site/portfolio-site/components/Grid.tsx
@@ -1,19 +1,25 @@
 
+import { g } from "motion/react-client";
 import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { gridItems } from "../data";
 
 const Grid = () => {
   return (
     <section id="about">
       <BentoGrid className="w-full py-20">
         
-        {[
-          { title: 'Title', description: 'Desc1', id:1 }
-        ].map((item) => (
+        {gridItems.map(({id, title, description, className, img, imgClassName, titleClassName,spareImg} ) => (
             <BentoGridItem
-            id={item.id}
-            key={item.id}
-            title={item.title}
-            description={item.description}
+            id={id}
+            key={id}
+            title={title}
+            description={description}
+            className={className}
+            titleClassName={titleClassName}
+            spareImg={spareImg}
+
+            img={img}
+            
             />
         ))}
       </BentoGrid>
